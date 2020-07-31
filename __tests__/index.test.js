8a29c5642fdc082d2627fc0420d81d204a6bfbd1
Paul Jensen <paulbjensen@gmail.com>
2020-07-31 10:38:03 +0100

    Added more tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,14 @@
 const assert = require('assert');
 const path = require('path');
 const mcg = require('../index');
-const { stat, rmdir, mkdir } = require('../lib/helpers');
+const { stat, rmdir, mkdir, readFile } = require('../lib/helpers');
+const { getTimestamp } = require('../lib/createRequiredFiles');
+const {
+	modelFileTemplate,
+	migrationFileTemplate,
+	testModelFileTemplate,
+	testSeedDataFileTemplate,
+} = require('../lib/templates');
 
 describe('main', () => {
 	const rootDir = path.join(process.cwd(), 'testApp');
@@ -14,6 +21,11 @@ describe('main', () => {
 	afterAll(async () => {
 		return await rmdir(rootDir, { recursive: true });
 	});
+
+	const checkFileExists = async (filePath) => {
+		const check = await stat(filePath);
+		assert(check.isFile());
+	};
 
 	const checkDirectoryExists = async (directories) => {
 		const fullPath = path.join(rootDir, ...directories);
@@ -45,14 +57,45 @@ describe('main', () => {
 		});
 	});
 
-	it.todo('should create the model file inside the models folder');
-	it.todo('should create the migration file inside the migrations folder');
+	it('should create the model file inside the models folder', async () => {
+		const filePath = path.join(rootDir, 'models', 'Post.js');
+		await checkFileExists(filePath);
+		const fileContent = await readFile(filePath);
+		const expectedContent = modelFileTemplate('Post');
+		assert.equal(fileContent, expectedContent);
+	});
 
-	it.todo(
-		'should create the test model file inside the __tests__/models/ folder'
-	);
+	it('should create the migration file inside the migrations folder', async () => {
+		const timestamp = getTimestamp();
+		const filePath = path.join(
+			rootDir,
+			'migrations',
+			`${timestamp}_create_posts_table.js`
+		);
+		await checkFileExists(filePath);
+		const fileContent = await readFile(filePath);
+		const expectedContent = migrationFileTemplate('posts');
+		assert.equal(fileContent, expectedContent);
+	});
 
-	it.todo(
-		'should create the test seed model file inside the __tests__/data/ folder'
-	);
+	it('should create the test model file inside the __tests__/models/ folder', async () => {
+		const filePath = path.join(
+			rootDir,
+			'__tests__',
+			'models',
+			'Post.test.js'
+		);
+		await checkFileExists(filePath);
+		const fileContent = await readFile(filePath);
+		const expectedContent = testModelFileTemplate('Post');
+		assert.equal(fileContent, expectedContent);
+	});
+
+	it('should create the test seed model file inside the __tests__/data/ folder', async () => {
+		const filePath = path.join(rootDir, '__tests__', 'data', 'seedPost.js');
+		await checkFileExists(filePath);
+		const fileContent = await readFile(filePath);
+		const expectedContent = testSeedDataFileTemplate('Post');
+		assert.equal(fileContent, expectedContent);
+	});
 });
