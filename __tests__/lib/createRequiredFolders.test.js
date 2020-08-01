948f8e06517ef364a304514b3eff9e62d5f12cb9
Paul Jensen <paulbjensen@gmail.com>
2020-08-01 20:08:30 +0100

    Added another test that was needed

--- __tests__/lib/createRequiredFolders.test.js
+++ __tests__/lib/createRequiredFolders.test.js
@@ -1,13 +1,20 @@
 describe('createRequiredFolders', () => {
-	it.todo('should create a models folder');
-	it.todo('should create a migrations folder');
-	it.todo('should create a __tests__ folder');
-	it.todo('should create a __tests__/models folder');
-	it.todo('should create a __tests__/data folder');
-	it.todo(
-		'should create the folders in a custom root directory if one is specified'
-	);
-	it.todo(
-		'should create the test-related folders with a custom test folder name if specified'
-	);
+	describe('#createRequiredFolders', () => {
+		it.todo('should create a models folder');
+		it.todo('should create a migrations folder');
+		it.todo('should create a __tests__ folder');
+		it.todo('should create a __tests__/models folder');
+		it.todo('should create a __tests__/data folder');
+		it.todo(
+			'should create the folders in a custom root directory if one is specified'
+		);
+		it.todo(
+			'should create the test-related folders with a custom test folder name if specified'
+		);
+	});
+
+	describe('#createFolderUnlessExists', () => {
+		it.todo('should create the folder if it does not exist');
+		it.todo('should not create the folder if it exists already');
+	});
 });
