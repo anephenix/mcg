217164329700a850077f72cd97f2fa199084bd9d
Paul Jensen <paulbjensen@gmail.com>
2020-08-03 09:31:14 +0100

    Added a test for the binary

--- __tests__/bin/mcg.test.js
+++ __tests__/bin/mcg.test.js
@@ -1,3 +1,32 @@
+const assert = require('assert');
+const path = require('path');
+const { exec, rmdir, stat, unlink } = require('../../lib/helpers');
+const { getTimestamp } = require('../../lib/createRequiredFiles');
+
 describe('mcg', () => {
-	it.todo('should generate the files and folders for a new model');
+	it('should generate the files and folders for a new model', async () => {
+		const command = './bin/mcg Post';
+		const { stdout } = await exec(command);
+		const timestamp = getTimestamp();
+		const filesToCheck = [
+			'__tests__/models/Post.test.js',
+			'__tests__/data/seedPost.js',
+			'models/Post.js',
+			`migrations/${timestamp}_create_posts_table.js`,
+		];
+		for await (const fileToCheck of filesToCheck) {
+			const filePath = path.join(process.cwd(), fileToCheck);
+			const fileCheck = await stat(filePath);
+			assert(fileCheck.isFile());
+			assert(stdout.match(filePath) !== null);
+			await unlink(filePath);
+		}
+		await rmdir(path.join(process.cwd(), 'models'), { recursive: true });
+		await rmdir(path.join(process.cwd(), 'migrations'), {
+			recursive: true,
+		});
+		await rmdir(path.join(process.cwd(), '__tests__', 'migrations'), {
+			recursive: true,
+		});
+	});
 });
