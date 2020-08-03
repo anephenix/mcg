9c76fdefc063c058f52f5f79d395281f4460d417
Paul Jensen <paulbjensen@gmail.com>
2020-08-03 17:10:04 +0100

    Added a test for passing a custom mainDir to the CLI

--- __tests__/bin/mcg.test.js
+++ __tests__/bin/mcg.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const path = require('path');
-const { exec, rmdir, stat, unlink } = require('../../lib/helpers');
+const { exec, mkdir, rmdir, stat, unlink } = require('../../lib/helpers');
 const { getTimestamp } = require('../../lib/createRequiredFiles');
 
 describe('mcg', () => {
@@ -51,8 +51,30 @@ describe('mcg', () => {
 		await rmdir(path.join(process.cwd(), 'migrations'), {
 			recursive: true,
 		});
-		await rmdir(path.join(process.cwd(), 'test', 'models'), {
+		await rmdir(path.join(process.cwd(), 'test'), {
 			recursive: true,
 		});
+	});
+
+	it('should generate the files in a custom main directory, if a custom main directory is passed', async () => {
+		const mainDir = path.join(process.cwd(),'sixthApp');
+		await mkdir(mainDir);
+		const command = `./bin/mcg Post --mainDir ${mainDir}`;
+		const { stdout } = await exec(command);
+		const timestamp = getTimestamp();
+		const filesToCheck = [
+			'__tests__/models/Post.test.js',
+			'__tests__/data/postData.test.js',
+			'models/Post.js',
+			`migrations/${timestamp}_create_posts_table.js`,
+		];
+		for await (const fileToCheck of filesToCheck) {
+			const filePath = path.join(mainDir, fileToCheck);
+			const fileCheck = await stat(filePath);
+			assert(fileCheck.isFile());
+			assert(stdout.match(filePath) !== null);
+			await unlink(filePath);
+		}
+		await rmdir(path.join(mainDir), { recursive: true });
 	});
 });
